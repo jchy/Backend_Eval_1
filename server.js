9a7742e865e5c9632f16041b1aefeed7121bc557
jchy <recjaswantcse@gmail.com>
2022-02-17 00:30:15 +0530

    _

--- server.js
+++ server.js
@@ -1,21 +1,9 @@
-// create Users, and Tweets Schema, Models
-// create routes, and use validations
-// create a CRUD application for a twitter application
-// the tweet should have title, body, tags (array), user_id
-// also add a users collections, keep username, email, id,
-// create CRUD for users, and tweets
-// /users - ( list all users ( with pagination) , create, delete, patch )
-// /tweets - ( list all tweets ( w pagination), list tweet by user (w pagination), create, delete, patch )
-// create an endpoint to search tweets by user_id, filter all tweets by a user
-// apply pagination on it with skip and limit
-// show total count as well, you can use .count() against the query
-
 const express=require('express');
 const app = express();
 const cors= require('cors');
-const connect= require('./config/db');
-const employeesRouter= require('./routes/employees.routes');
-const expensesRouter= require('./routes/expenses.route');
+const connect= require('./src/config/db');
+const employeesRouter= require('./src/routes/employees.routes');
+const expensesRouter= require('./src/routes/expenses.route');
 
 const PORT = 5001;
 
