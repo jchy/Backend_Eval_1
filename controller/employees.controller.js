9f6adc15481367a3dce6881b02f66ba732fb9452
jchy <recjaswantcse@gmail.com>
2022-02-17 00:23:23 +0530

    _

---
+++ controller/employees.controller.js
@@ -0,0 +1,109 @@
+const express = require('express');
+const {body , validationResult} = require('express-validator');
+
+const router= express.Router();
+
+const Employee= require('../models/employees.model');
+const validateEmployee = require("../utils/validateEmployee");
+
+// ? pagination
+// ? limit, skip
+
+const getAllEmployees =  async (req,res)=>{
+    try{
+        const per_page = req.query.per_page || 2;
+        const page = req.query.page || 1;
+        const skip = page < 0 ? 0 : (page - 1)*per_page;
+        // (page - 1)*per_page
+
+        const employees = await Employee.find().skip(skip).limit(per_page);
+
+        if(!employees) return res.status(400).json({msg: "No employees found"}) 
+        res.status(200).json(employees);
+    }
+    catch(err){
+        return res.status(400).json({msg: "Something went wrong!"})
+    }
+}
+
+const getEmployeeByName =  async (req,res)=>{
+    try{
+        const employees = await Employee.findOne({ name: req.params.name});
+        if(!employees) return res.status(400).json({msg: "Employees not found"})        
+        res.status(200).json(employees);
+    }
+    catch(err){
+        return res.status(400).json({msg: "Something went wrong!"})
+    }
+}
+
+
+const createEmployee = async (req,res)=>{
+    try{
+        // * Validate
+        const errors = validationResult(req);
+        if(!errors.isEmpty())
+        {
+            return res.status(400).json({errors: errors.array()});
+        }
+        // * Create Employee
+        const doesEmployeeExist = await Employee.findOne({name: req.body.name})
+        console.log("jhgjfdghjkgfdx");
+        if(doesEmployeeExist) return res.status(400).json({msg: "Duplicate Employee found"})
+        const employees = await Employee.create({
+            employee_id : req.body.employee_id,
+            name: req.body.name,
+            gender : req.body.gender
+        })
+        console.log(employees);
+        if(!employees) return res.status(400).json({msg: "Employee not created"})
+
+        //200 ok
+        return res.status(200).json(employees)
+    }
+    catch(err){
+        return res.status(400).json({msg: "Something went wrong!"})
+    }
+}
+
+const removeEmployee = async (req,res)=>{
+    try{
+        const employees = await Employee.findOneAndDelete({ _id: req.params.employees_id })
+        if(!employees) return res.status(404).json({msg: "Employee not found"})
+        res.status(200).json(employees)
+    }
+    catch(err){
+        return res.status(400).json({msg: "Something went wrong!"})
+    }
+}
+
+const updateEmplyeeData = async (req,res)=>{
+    try{
+        if(!req.body.name) return res.status(400).json({msg: "Name is required"});
+        const employees = await Employee.findOneAndUpdate({ 
+            _id: req.params.employees_id 
+        },{
+            $set: {
+                name: req.body.name,
+                gender: req.body.gender
+            }
+        },{
+            returnOriginal: false
+        }
+            )
+        if(!employees) return res.status(404).json({msg: "Employee not found"})
+        res.status(200).json(employees)
+    }
+    catch(err){
+        return res.status(400).json({msg: "Something went wrong!"})
+    }
+}
+
+
+module.exports = {
+    getAllEmployees,
+    getEmployeeByName,
+    createEmployee,
+    removeEmployee,
+    updateEmplyeeData
+}
--- routes/expenses.route.js
+++ routes/expenses.route.js
@@ -6,160 +6,35 @@ const router= express.Router();
 
 const Expenses = require('../models/expenses.model');
 const validateExpenses = require("../utils/validateExpenses");
-
+const { getAllExpenses,
+    getExpensesByUserId,
+    getExpenses_Bw_Date1_and_Date2_sortby_asc,
+    getAvgeExpenseReimburse,
+    groupExpenseByTypeAndSort,
+    createExpenses,
+    removeExpenseByEmployeeId,
+    updateExpenses} = require('../controller/expenses.controller');
 // ? pagination
 // ? limit, skip
 
-router.get("/", async (req,res)=>{
-    try{
-        const per_page = req.query.per_page || 2;
-        const page = req.query.page || 1;
-        const skip = page < 0 ? 0 : (page - 1)*per_page;
-        // (page - 1)*per_page
-        const expenses = await Expenses.find().skip(skip).limit(per_page);
-        if(!expenses) return res.status(400).json({msg: "No Expense is found"}) 
-        res.status(200).json(expenses);
-    }
-    catch(err){
-        return res.status(400).json({msg: "Something went wrong!"})
-    }
-})
+router.get("/", getAllExpenses);
 
-router.get("/groupbytype", async (req,res)=>{
-    try{
-        const per_page = req.query.per_page || 2;
-        const page = req.query.page || 1;
-        const skip = page < 0 ? 0 : (page - 1)*per_page;
-        // (page - 1)*per_page
-        const expenses = await Expenses.aggregate([
-            {
-              $group: {
-                _id : { type : "$type" }, count: { $count:{}}
-              }
-            }, { $sort :  { count : 1}}
-          ])
-        if(!expenses) return res.status(400).json({msg: "No Expense is found"}) 
-        res.status(200).json(expenses);
-    }
-    catch(err){
-        return res.status(400).json({msg: "Something went wrong!"})
-    }
-})
+router.get("/groupbytype", groupExpenseByTypeAndSort);
 
-router.get("/avgeexpensereimburse", async (req,res)=>{
-    try{
-        const per_page = req.query.per_page || 2;
-        const page = req.query.page || 1;
-        const skip = page < 0 ? 0 : (page - 1)*per_page;
-        // (page - 1)*per_page
-        const expenses = await Expenses.aggregate([{$match: {}},{$group: { _id: "$reimbursed_date" }}])
-         
-        if(!expenses) return res.status(400).json({msg: "No Expense is found"}) 
-        res.status(200).json(expenses);
-    }
-    catch(err){
-        return res.status(400).json({msg: "Something went wrong!"})
-    }
-})
+router.get("/avgeexpensereimburse", getAvgeExpenseReimburse);
 
-router.get("/sortbydate/:asc", async (req,res)=>{
-    try{
-        const per_page = req.query.per_page || 2;
-        const page = req.query.page || 1;
-        const skip = page < 0 ? 0 : (page - 1)*per_page;
-        // (page - 1)*per_page
-        const date1 = req.query.date1 ;
-        const date2 = req.query.date2 ;
-        console.log(date1, date2);
-        const expenses = await Expenses.find({date_of_expense:{$gt:Number(date1),$lt:Number(date2)}}).sort({date_of_expense : req.params.asc }).skip(skip).limit(per_page);
-        if(!expenses) return res.status(400).json({msg: "No Expense is found"}) 
-        res.status(200).json(expenses);
-    }
-    catch(err){
-        return res.status(400).json({msg: "Something went wrong!"})
-    }
-})
+router.get("/sortbydate/:asc", getExpenses_Bw_Date1_and_Date2_sortby_asc);
 
 
 
 
-router.get("/:user_id", async (req,res)=>{
-    try{
-        const expenses = await Expenses.findOne({_id: req.params.user_id});
-        if(!expenses) return res.status(400).json({msg: "Expenses not found"})        
-        res.status(200).json(expenses);
-    }
-    catch(err){
-        return res.status(400).json({msg: "Something went wrong!"})
-    }
-})
+router.get("/:user_id", getExpensesByUserId);
 
 
-router.post("/", ...validateExpenses() ,async (req,res)=>{
-    try{
-        // * Validate
-        const errors = validationResult(req);
-        if(!errors.isEmpty())
-        {
-            return res.status(400).json({errors: errors.array()});
-        }
+router.post("/", ...validateExpenses() , createExpenses);
 
-        // * Create User
-        const doesExpensesExist= await Expenses.findOne({employee_id : req.body.employee_id})
-        if(doesExpensesExist) return res.status(400).json({msg: "Duplicate Expense found"})
-        const expenses = await Expenses.create({
-            type: req.body.type,
-            date_of_expense: req.body.date_of_expense,
-            employee_id: req.body.employee_id,
-            reimbursed: req.body.reimbursed,
-            reimbursed_date: req.body.reimbursed_date
-        })
+router.delete("/:employee_id", removeExpenseByEmployeeId)
 
-        if(!expenses) return res.status(400).json({msg: "Expense not created"})
-
-        //200 ok
-        return res.status(200).json(expenses)
-    }
-    catch(err){
-        return res.status(400).json({msg: "Something went wrong!"})
-    }
-})
-
-router.delete("/:employee_id", async (req,res)=>{
-    try{
-        const expenses = await Expenses.findOneAndDelete({ _id: req.params.employee_id })
-        if(!expenses) return res.status(404).json({msg: "Expenses is not found"})
-        res.status(200).json(expenses)
-    }
-    catch(err){
-        return res.status(400).json({msg: "Something went wrong!"})
-    }
-})
-
-router.patch("/:employee_id", async (req,res)=>{
-    try{
-        if(!req.body.title) return res.status(400).json({msg: "Title is required"});
-        const expenses = await Expenses.findOneAndUpdate({ 
-            _id: req.params.employee_id 
-        },{
-            $set: {
-                type: req.body.type,
-                date_of_exppense: req.body.date_of_exppense,
-                employee_id: req.body.employee_id,
-                reimbursed: req.body.reimbursed,
-                reimbursed_date: req.body.reimbursed_date,
-            }
-        },{
-            returnOriginal: false
-        }
-            )
-        if(!expenses) return res.status(404).json({msg: "Expenses is not found"})
-        res.status(200).json(expenses)
-    }
-    catch(err){
-        return res.status(400).json({msg: "Something went wrong!"})
-    }
-})
-
+router.patch("/:employee_id", updateExpenses)
 
 module.exports = router;
